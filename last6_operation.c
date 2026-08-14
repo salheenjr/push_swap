6ad0760bacff96e186b3ee4b53885e839dd4671b
Salheen Alagore <saalagor@u91z04s05.42kl.edu.my>
2026-08-14 10:38:39 +0800

    Modifying rra and rrb by putting last->next->next

--- last6_operation.c
+++ last6_operation.c
@@ -6,7 +6,7 @@
 /*   By: wchan-ha <wchan-ha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/14 08:57:25 by wchan-ha          #+#    #+#             */
-/*   Updated: 2026/08/14 10:15:13 by wchan-ha         ###   ########.fr       */
+/*   Updated: 2026/08/14 10:37:28 by saalagor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,53 +53,31 @@ void	rr(a_list **a, a_list **b)
 void	rra(ab_list	**a)
 {
 	ab_list	last;
-	ab_list	last_before;
-	int	len;
-	int	i;
 
 	if (!a || !*a || !(*a)->next)
 		return ;
 	last = *a;
-	len = 0;
-	while (last->next)
+	while (last->next->next)
 	{
 		last = last->next;
-		len++;
 	}
-	i = 0;
-	while (last_before->next && i < len - 1)
-	{
-		last_before = last_before->next;
-		i++;
-	}
-	*a = last;
-	last_before->next = NULL;
+	*a = last->next;
+	last->next = NULL;
 }
 
 void	rrb(ab_list	**b)
 {
 	ab_list	last;
-	ab_list	last_before;
-	int	len;
-	int	i;
 
 	if (!b || !*b || !(*b)->next)
 		return ;
 	last = *b;
-	len = 0;
-	while (last->next)
+	while (last->next->next)
 	{
 		last = last->next;
-		len++;
 	}
-	i = 0;
-	while (last_before->next && i < len - 1)
-	{
-		last_before = last_before->next;
-		i++;
-	}
-	*b = last;
-	last_before->next = NULL;
+	*b = last->next;
+	last->next = NULL;
 }
 
 void	rrr(ab_list **a, ab_list **b)
