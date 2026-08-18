98c2244f1f948982d7ce9c4c3476dbfc3fc8bb3a
Wong Chan Hao <wchan-ha@student.42kl.edu.my>
2026-08-18 15:56:09 +0800

    draft notes about complex algo

---
+++ srcs/sorting/sort_complex.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_complex.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wchan-ha <wchan-ha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/18 14:43:56 by wchan-ha          #+#    #+#             */
+/*   Updated: 2026/08/18 15:54:50 by wchan-ha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+void	sort_complex(t_stack **a, t_stack **b)
+{
+
+// Turk Sorting
+
+// 1. throw 2 number
+// 2. a comparison < b lesser but greater than other
+// 3. push a to b
+// 4. biggest throw it back,
+// 5. if is the smallest throw it and rotate
+
+// 5. assign index, then ra, or rra
+// 6. for the cost couting, index / 2
+// 7. until reach 3
+// 8. move from stack b back to a
+// 9. if not in order reverse rotate stack a
+}
